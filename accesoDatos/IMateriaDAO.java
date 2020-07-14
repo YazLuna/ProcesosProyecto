16e5bddf2bdab22cefe1ace0880533666e55d3c5
MARTHA <martha_15_7@outlook.com>
2020-07-14 00:11:34 -0500

    termine mi implementacion

--- accesoDatos/IMateriaDAO.java
+++ accesoDatos/IMateriaDAO.java
@@ -5,6 +5,6 @@ import dominio.Materia;
 
 public interface IMateriaDAO {
     public List<Materia> getListMateria ();
-    public boolean registrarUsuarioMateria(int NRC,int idUsuario);
+    public boolean registrarUsuarioMateria(int NRC,String RFC);
     public boolean registrarMateria(int NRC,String nombre,String descripcion,String area,String periodo,String turno);
 }
